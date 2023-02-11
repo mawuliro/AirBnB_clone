8b5777a7e474104a0a7b77e90670957d985b1ece
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-02-11 23:15:36 +0000

    AirBnB project, database

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+"""Defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represent an amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
